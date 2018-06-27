734c98507341747916223ceab91cdb99b1bb5521
kreezxil <kreezxil@gmail.com>
2018-06-27 17:29:53 -0500

    3rd Commit
    I forgot how much I hate model work. I'll try a model editor before the 4th commit. I need a fast way to do this stupid stuff!

--- src/main/java/com/kreezcraft/dirtdeco/blocks/InitBlocks.java
+++ src/main/java/com/kreezcraft/dirtdeco/blocks/InitBlocks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 
@@ -17,8 +18,14 @@ public class InitBlocks {
 
 	
 	public static final Block DIRT_SLAB = new ModSlab("dirt_slab",Blocks.DIRT);
-	public static final Block DIRT_STAIRS = new ModStairs("dirt_stores",Blocks.DIRT);
+	public static final Block DIRT_STAIRS = new ModStairs("dirt_stairs",Blocks.DIRT);
+	public static final Block DIRT_FENCE = new ModFence("dirt_fence",Material.GROUND);
+	public static final Block DIRT_DOOR = new ModDoor("dirt_door", Material.GROUND);
+	public static final Block DIRT_TRAP_DOOR = new ModTrapDoor("dirt_trap_door", Material.GROUND);
 	
 	public static final Block GRASS_SLAB = new ModSlab("grass_slab",Blocks.GRASS);
 	public static final Block GRASS_STAIRS = new ModStairs("grass_stairs",Blocks.GRASS);
+	public static final Block GRASS_FENCE = new ModFence("grass_fence",Material.GRASS);
+	public static final Block GRASS_DOOR = new ModDoor("grass_door", Material.GRASS);
+	public static final Block GRASS_TRAP_DOOR = new ModTrapDoor("grass_trap_door", Material.GRASS);
 }
