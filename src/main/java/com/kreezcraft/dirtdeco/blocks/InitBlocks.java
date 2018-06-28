06ea655dc32ed7ff3fd63ee3b8947e59ded13b56
kreezxil <kreezxil@gmail.com>
2018-06-28 16:10:40 -0500

    #4!!!
    a couple more classes
    a couple more models
    worked out some texture bugs
    still more model work to do

--- src/main/java/com/kreezcraft/dirtdeco/blocks/InitBlocks.java
+++ src/main/java/com/kreezcraft/dirtdeco/blocks/InitBlocks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockPlanks.EnumType;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
@@ -12,20 +13,17 @@ public class InitBlocks {
 
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-//	public static final Block DIAMOND_SANDSTONE = new BlockStandard("diamond_sandstone");
-//	public static final Block DIAMOND_SMOOTH_SANDSTONE = new BlockStandard("diamond_smooth_sandstone");
-//	public static final Block DIAMOND_CHISELED_SANDSTONE = new BlockStandard("diamond_chiseled_sandstone");
-
-	
 	public static final Block DIRT_SLAB = new ModSlab("dirt_slab",Blocks.DIRT);
 	public static final Block DIRT_STAIRS = new ModStairs("dirt_stairs",Blocks.DIRT);
 	public static final Block DIRT_FENCE = new ModFence("dirt_fence",Material.GROUND);
 	public static final Block DIRT_DOOR = new ModDoor("dirt_door", Material.GROUND);
 	public static final Block DIRT_TRAP_DOOR = new ModTrapDoor("dirt_trap_door", Material.GROUND);
+	public static final Block DIRT_GATE = new ModGate("dirt_gate", EnumType.JUNGLE );
 	
 	public static final Block GRASS_SLAB = new ModSlab("grass_slab",Blocks.GRASS);
 	public static final Block GRASS_STAIRS = new ModStairs("grass_stairs",Blocks.GRASS);
 	public static final Block GRASS_FENCE = new ModFence("grass_fence",Material.GRASS);
 	public static final Block GRASS_DOOR = new ModDoor("grass_door", Material.GRASS);
 	public static final Block GRASS_TRAP_DOOR = new ModTrapDoor("grass_trap_door", Material.GRASS);
+	public static final Block GRASS_GATE = new ModGate("grass_gate", EnumType.JUNGLE);
 }
---
+++ src/main/java/com/kreezcraft/dirtdeco/blocks/ModGate.java
@@ -0,0 +1,35 @@
+package com.kreezcraft.dirtdeco.blocks;
+
+import com.kreezcraft.dirtdeco.DirtDeco;
+import com.kreezcraft.dirtdeco.DirtDecoConfig;
+import com.kreezcraft.dirtdeco.client.IHasModel;
+import com.kreezcraft.dirtdeco.items.InitItems;
+
+import net.minecraft.block.BlockFenceGate;
+import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class ModGate extends BlockFenceGate implements IHasModel {
+
+	private String name;
+
+	public ModGate(String name, EnumType p_i46394_1_) {
+		super(p_i46394_1_);
+		this.name = name;
+		setCreativeTab(DirtDeco.creativeTab);
+		setUnlocalizedName(DirtDeco.MODID + "." + name);
+		setRegistryName(name);
+		setHardness(DirtDecoConfig.nondiamondLevel.standardHardness);
+		setResistance(DirtDecoConfig.nondiamondLevel.standardResistance);
+		useNeighborBrightness = true;
+		InitBlocks.BLOCKS.add(this);
+		InitItems.ITEMS.add(new ItemBlock(this).setRegistryName(getRegistryName()));
+	}
+
+	@Override
+	public void registerModels() {
+		DirtDeco.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+
+}
