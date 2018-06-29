7d0b0b86f901b3f22421bd905f2e38665c1343dc
kreezxil <kreezxil@gmail.com>
2018-06-29 11:59:37 -0500

    Mambo #5
    [Intro]
    Ladies and gentlemen
    This is Mambo No. 5!
    
    [Verse 1]
    One, two, three, four, five
    Everybody in the car, so come on let's ride
    To the liquor store around the corner
    The boys say they want some gin and juice, but I really don't wanna
    Beer bust, like I had last week
    I must stay deep, cause talk is cheap
    I like Angela, Pamela, Sandra and Rita
    And as I continue you know they're getting sweeter
    So what can I do? I really beg you, my Lord
    To me flirting is just like a sport
    Anything fly, it's all good let me dump it
    Please set in the trumpet
    
    [Hook]
    A little bit of Monica in my life
    A little bit of Erica by my side
    A little bit of Rita is all I need
    A little bit of Tina is what I see
    A little bit of Sandra in the sun
    A little bit of Mary all night long
    A little bit of Jessica here I am
    A little bit of you makes me your man
    
    Mambo No. 5!
    
    [Verse 2]
    Jump up and down and move it all around
    Shake your head to the sound
    Put your hands on the ground
    Take one step left and one step right
    One to the front and one to the side
    Clap your hands once and clap your hands twice
    And if it looks like this then you're doing it right
    
    [Hook]
    A little bit of Monica in my life
    A little bit of Erica by my side
    A little bit of Rita is all I need
    A little bit of Tina is what I see
    A little bit of Sandra in the sun
    A little bit of Mary all night long
    A little bit of Jessica here I am
    A little bit of you makes me your man
    
    [Bridge]
    Trumpet!
    The trumpet!
    Mambo No. 5!
    (ha ha ha)
    
    [Hook]
    A little bit of Monica in my life
    A little bit of Erica by my side
    A little bit of Rita is all I need
    A little bit of Tina is what I see
    A little bit of Sandra in the sun
    A little bit of Mary all night long
    A little bit of Jessica here I am
    A little bit of you makes me your man
    
    [Outro]
    I do all I do
    To fall in love with a girl like you
    You can't run and you can't hide
    You and me gonna touch the sky
    
    Mambo No. 5!

--- src/main/java/com/kreezcraft/dirtdeco/blocks/ModDoor.java
+++ src/main/java/com/kreezcraft/dirtdeco/blocks/ModDoor.java
@@ -1,5 +1,7 @@
 package com.kreezcraft.dirtdeco.blocks;
 
+import java.util.Random;
+
 import com.kreezcraft.dirtdeco.DirtDeco;
 import com.kreezcraft.dirtdeco.DirtDecoConfig;
 import com.kreezcraft.dirtdeco.client.IHasModel;
@@ -9,10 +11,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
-public class ModDoor extends BlockDoor implements IHasModel{
+public class ModDoor extends BlockDoor implements IHasModel {
 
 	private String name;
 
@@ -34,4 +38,8 @@ public class ModDoor extends BlockDoor implements IHasModel{
 		DirtDeco.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
 	}
 
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return state.getValue(HALF) == BlockDoor.EnumDoorHalf.UPPER ? Items.AIR : Item.getItemFromBlock(this);
+	}
 }
--- src/main/java/com/kreezcraft/dirtdeco/items/InitItems.java
+++ src/main/java/com/kreezcraft/dirtdeco/items/InitItems.java
@@ -3,6 +3,8 @@ package com.kreezcraft.dirtdeco.items;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.kreezcraft.dirtdeco.DirtDeco;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
@@ -10,6 +12,7 @@ public class InitItems {
 
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 
-	//public static final Item DIAMONIUM = new ItemBase("diamonium").setCreativeTab(CreativeTabs.MATERIALS);
+//	public static final Item DIRT_DOOR = new ItemBase("dirt_door").setCreativeTab(DirtDeco.creativeTab);
+//	public static final Item GRASS_DOOR = new ItemBase("grass_door").setCreativeTab(DirtDeco.creativeTab);
 
 }
